de8681d2ce12a06fe3f0596a6f61af010ac8a0f2
zhou <zzy28101994@gmail.com>
2018-10-16 23:36:12 +0200

    2

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
