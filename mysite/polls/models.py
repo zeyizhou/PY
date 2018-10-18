246d646e3f13d66d77fe3747f07f3c7d77ff9a8a
zhou <zzy28101994@gmail.com>
2018-10-18 23:31:40 +0200

    add style html test

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import datetime
-from django.db import models
+
 from django.db import models
 from django.utils import timezone
 # Create your models here.
