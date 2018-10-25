2af6cdd534cd54f32a4f91782b67550c885d5d20
zhou <zzy28101994@gmail.com>
2018-10-25 19:50:44 +0200

    deploy

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -128,3 +128,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 FILE_UPLOAD_HANDLERS = ("django_excel.ExcelMemoryFileUploadHandler",
                         "django_excel.TemporaryExcelFileUploadHandler")
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
